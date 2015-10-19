29232cf01c845d991b11dc1c0ec9f95f88af6321
wom2-wombat <wom2.wombat@gmail.com>
2015-10-19 11:51:31 +0900

    Exec tmux on starting zsh

--- script/make_links.sh
+++ script/make_links.sh
@@ -5,5 +5,6 @@ dotfiles_dir="${HOME}/dotfiles"
 
 # Make links from dotfiles to HOME
 ln -s ${dotfiles_dir}/zshrc $HOME/.zshrc
+ln -s ${dotfiles_dir}/zsh $HOME/.zsh
 ln -s ${dotfiles_dir}/vimrc $HOME/.vimrc
 ln -s ${dotfiles_dir}/vim $HOME/.vim
