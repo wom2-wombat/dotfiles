b7117dd4a1c7789da35cb00eefc5e5bb801bd456
wom2-wombat <wom2.wombat@gmail.com>
2015-10-20 04:29:16 +0900

    Reduce key repeat wait

--- script/karabiner.sh
+++ script/karabiner.sh
@@ -2,6 +2,10 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set repeat.wait 20
+/bin/echo -n .
+$cli set repeat.initial_wait 100
+/bin/echo -n .
 $cli set option.emacsmode_controlPNBF_nomodifiers 1
 /bin/echo -n .
 /bin/echo
