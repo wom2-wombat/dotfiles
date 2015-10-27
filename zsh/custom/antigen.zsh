cdd9c6addca1d6f1df6d2ef732fc8924fd2b9d08
wom2-wombat <wom2.wombat@gmail.com>
2015-10-28 08:04:45 +0900

    Install and enable zaw on zsh

--- zsh/custom/antigen.zsh
+++ zsh/custom/antigen.zsh
@@ -6,5 +6,8 @@ antigen use oh-my-zsh
 # Syntax highlighting bundle.
 antigen bundle zsh-users/zsh-syntax-highlighting
 
+# UI
+antigen bundle zsh-users/zaw
+
 # Tell antigen that you're done.
 antigen apply
