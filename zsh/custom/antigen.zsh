4ceff0ca674acdbef0079936eacc87012a762723
wom2-wombat <wom2.wombat@gmail.com>
2015-10-28 07:47:40 +0900

    Enable antigen on zsh

---
+++ zsh/custom/antigen.zsh
@@ -0,0 +1,10 @@
+source ~/dotfiles/antigen/antigen.zsh
+
+# Load the oh-my-zsh's library.
+antigen use oh-my-zsh
+
+# Syntax highlighting bundle.
+antigen bundle zsh-users/zsh-syntax-highlighting
+
+# Tell antigen that you're done.
+antigen apply
